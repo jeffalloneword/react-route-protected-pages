a9509a2b12fd3c88a22e4f81591bde664af0c140
Jeff Hyatt <jhyatt00@gmail.com>
2018-07-31 16:23:29 -0400

    setup fake auth to test routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) //fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
+
+const Public = () => <h3>Public</h3>
+const Protected = () => <h3>Protected</h3>
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        LOGIN
+      </div>
+    )
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+    ? <Component {...props} />
+    : <Redirect to='/login' />
+  )}/>
+)
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Protected Pages Example
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to='/public'>Public Page</Link></li>
+            <li><Link to='/protected'>Protected Page</Link></li>
+          </ul>
+
+          <Route path='/public' component={Public} />
+          <Route path='/login' component={Login} />
+          <PrivateRoute path='/protected' component={Protected} />
+        </div>
+      </Router>
     );
   }
 }
