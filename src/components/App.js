c95ac560a05b5e2da9fdd18fc49dabc449c726c1
Jeff Hyatt <jhyatt00@gmail.com>
2018-07-31 15:27:15 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        React Router Protected Pages Example
+      </div>
+    );
+  }
+}
+
+export default App;
